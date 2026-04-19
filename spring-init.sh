b0b4840c80a961cbe648a5196a924fd7b60f3d0b
Stefan Gamerith <steff7@gmx.com>
2026-04-19 14:01:34 +0200

    First draft of Spring init script

---
+++ spring-init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+spring init \
+	--bootVersion=4.0.5 \
+	--configurationFileFormat=yaml \
+	--language=java \
+	--build:gradle,dialect:kotlin,format:project \
+	--javaVersion=25 \
+	--dependencies=web,data-jpa,postgresql,lombok,validation,actuator,testcontainers,flyway \
+	--groupId=at.philatelist \
+	--packageName=at.philatelist.$1 \
+	--artifactId=$1 \
+$1
